e63c094dfca8ac38343b52ec6b991e91652e0def
mushfikur-dwip <musfikurrahmandip@gmail.com>
2025-03-03 01:37:13 +0600

    Blog Page Completed with Js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
+
 const completeButtons = document.getElementsByClassName("complete-btn");
 Array.from(completeButtons).forEach((button) => {
   button.addEventListener("click", function () {
     this.disabled = true;
-
     const taskTitle =
       this.closest(".card").querySelector(".card-title").innerText;
 
@@ -39,20 +39,63 @@ const clearHistoryButton = document.querySelector(".btn.clear-history");
 clearHistoryButton.addEventListener("click", function () {
   const activityLogContainer = document.querySelector(
     ".activity-log-container"
-  ); 
-  activityLogContainer.innerHTML = ""; 
+  );
+  activityLogContainer.innerHTML = "";
 });
-
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
+    console.log(color);
   }
   return color;
 }
 
 function changeButtonColor() {
-    document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
 }
+
+function displayCurrentDate() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const currentTime = new Date();
+  const dayToday = daysOfWeek[currentTime.getDay()];
+  const monthToday = monthsOfYear[currentTime.getMonth()];
+  const day = currentTime.getDate();
+  const yearToday = currentTime.getFullYear();
+
+  document.getElementById("currentDay").innerText = dayToday.slice(0, 3);
+  document.getElementById("currentMonth").innerText =
+    monthToday.slice(0, 3) + " " + day + " " + yearToday;
+}
+displayCurrentDate();
+
+
+document.getElementById("blog-page-btn").addEventListener("click", function () {
+  window.location.assign("../blog.html");
+});
---
+++ js/blog.js
@@ -0,0 +1,3 @@
+document.getElementById("back-btn").addEventListener("click", function () {
+  window.location.assign("../index.html");
+});
